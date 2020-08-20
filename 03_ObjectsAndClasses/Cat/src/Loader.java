0aec06e9f1e97efbdf56a2f62be398f353619247
Andrey Kurochkin <barbedak@gmail.com>
2020-08-20 15:03:37 +0300

    experiments on cats

--- 03_ObjectsAndClasses/Cat/src/Loader.java
+++ 03_ObjectsAndClasses/Cat/src/Loader.java
@@ -3,8 +3,46 @@ public class Loader
 {
     public static void main(String[] args)
     {
-        Cat cat = new Cat();
+        Cat barsik = new Cat();
+        Cat murzik = new Cat();
+        Cat murka = new Cat();
+        Cat kenny = new Cat();
+        Cat musya = new Cat();
+        Cat chubays = new Cat();
 
-        System.out.println(cat.getStatus());
+        System.out.println("Barsik weight: " + barsik.getWeight());
+        System.out.println("Murzik weight: " + murzik.getWeight());
+        System.out.println("Murka weight: " + murka.getWeight());
+        System.out.println("Kenny weight: " + kenny.getWeight());
+        System.out.println("Musya weight: " + musya.getWeight());
+        System.out.println("Chubays weight: " + chubays.getWeight());
+
+        //feeding cats
+        System.out.println();
+        System.out.println("Feeding Barsik and Murzik");
+        barsik.feed(100.0);
+        murzik.feed(100.0);
+        System.out.println("Now Barsik weight: " + barsik.getWeight());
+        System.out.println("Now Murzik weight: " + murzik.getWeight());
+
+        //feeding Murka
+        System.out.println();
+        System.out.println("Murka status: " + murka.getStatus());
+        System.out.println("Feeding Murka");
+        while (!murka.getStatus().equals("Exploded")){
+            murka.feed(100.0);
+        }
+        System.out.println("Murka status: " + murka.getStatus());
+
+        //killing Kenny
+        System.out.println();
+        System.out.println("Kenny status: " + kenny.getStatus());
+
+        while (!kenny.getStatus().equals("Dead")){
+            System.out.print("Kenny says ");
+            kenny.meow();
+        }
+        System.out.println("Kenny is "+ kenny.getStatus() + " :(");
+
     }
 }
