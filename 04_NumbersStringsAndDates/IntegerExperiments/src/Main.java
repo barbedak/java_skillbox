ea28ad1ebcd3bbf72a03bbf8f58743afe0e6c8b6
Andrey Kurochkin <barbedak@gmail.com>
2020-09-07 11:21:40 +0300

    lesson 4.1 Fix method & calculation sum of digits

--- 04_NumbersStringsAndDates/IntegerExperiments/src/Main.java
+++ 04_NumbersStringsAndDates/IntegerExperiments/src/Main.java
@@ -5,11 +5,22 @@ public class Main
         Container container = new Container();
         container.count += 7843;
 
+        System.out.println(sumDigits(12345));
+        System.out.println(sumDigits(10));
+        System.out.println(sumDigits(5059191));
+
+
     }
 
-    public Integer sumDigits(Integer number)
+    public static Integer sumDigits(Integer number)
     {
         //@TODO: write code here
-        return 0;
+        int sum = 0;
+        Character ch;
+        for (int i = 0; i < number.toString().length(); i++) {
+            ch = number.toString().charAt(i);
+            sum = sum + Character.getNumericValue(ch);
+        }
+        return sum;
     }
 }
