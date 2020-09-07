523b45a3191e3a51103219c55d0a1ccbba4620b9
Andrey Kurochkin <barbedak@gmail.com>
2020-09-07 14:53:52 +0300

    lesson 4.2 max min values

---
+++ 04_NumbersStringsAndDates/MinMaxVolume/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Min Byte : " + Byte.MIN_VALUE);
+        System.out.println("Max Byte : " + Byte.MAX_VALUE);
+
+        System.out.println("Min Integer : " + Integer.MIN_VALUE);
+        System.out.println("Max Integer : " + Integer.MAX_VALUE);
+
+        System.out.println("Min Short : " + Short.MIN_VALUE);
+        System.out.println("Max Short : " + Short.MAX_VALUE);
+
+        System.out.println("Min Long : " + Long.MIN_VALUE);
+        System.out.println("Max Long : " + Long.MAX_VALUE);
+
+        System.out.println("Min Float : " + Float.MIN_VALUE);
+        System.out.println("Max Float : " + Float.MAX_VALUE);
+
+        System.out.println("Min Double : " + Double.MIN_VALUE);
+        System.out.println("Max Double : " + Double.MAX_VALUE);
+
+    }
+}
