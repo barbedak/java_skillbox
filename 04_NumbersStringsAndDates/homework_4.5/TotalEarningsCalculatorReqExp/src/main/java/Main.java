e87001c32aa8d3a41bcf8edfb456eab94e84d966
Andrey Kurochkin <barbedak@gmail.com>
2020-11-19 14:14:49 +0300

    homework 4.5 TotalEarningsCalculatorReqExp pattern & mather

--- 04_NumbersStringsAndDates/homework_4.5/TotalEarningsCalculatorReqExp/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/TotalEarningsCalculatorReqExp/src/main/java/Main.java
@@ -1,21 +1,19 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
   public static void main(String[] args) {
-
   }
 
   public static int calculateSalarySum(String text){
     //TODO: реализуйте метод
-    text = text.replaceAll("[^0-9]+", " ").trim();
-
-    if (text.length() == 0) {
-      return 0;
-    }
-
-    String[] salary = text.split(" ");
     int sum = 0;
-    for (String s : salary) {
-      sum = sum + Integer.parseInt(s);
+    Pattern pattern = Pattern.compile("\\d+");
+    Matcher matcher = pattern.matcher(text);
+
+    while (matcher.find()){
+      sum += Integer.parseInt(matcher.group());
     }
     return sum;
   }
