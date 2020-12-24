dbce2f9e33d1c25b258eba49f2218a6866d79721
Andrey Kurochkin <barbedak@gmail.com>
2020-12-24 14:53:51 +0300

    homework 4.6 BirthdayCalendar fix birthday 29.02

--- 04_NumbersStringsAndDates/homework_4.6/BirthdayCalendar/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.6/BirthdayCalendar/src/main/java/Main.java
@@ -7,9 +7,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int day = 31;
-        int month = 12;
-        int year = 1990;
+        int day = 29;
+        int month = 2;
+        int year = 1992;
 
         System.out.println(collectBirthdays(year, month, day));
 
@@ -21,13 +21,16 @@ public class Main {
         //0 - 31.12.1990 - Mon
         //1 - 31.12.1991 - Tue
 
-        Calendar birthday = new GregorianCalendar(year, month-1, day);
+        GregorianCalendar birthday = new GregorianCalendar(year, month-1, day);
         Calendar now = Calendar.getInstance();
         SimpleDateFormat dateFormat = new SimpleDateFormat(" - dd.MM.yyyy - E", Locale.US);
         String text = "";
 
         int i = 0;
         while (!birthday.after(now)) {
+            if (birthday.isLeapYear(birthday.get(Calendar.YEAR))) {
+                birthday.set(Calendar.DAY_OF_MONTH, day);
+            }
             text = text.concat(i + dateFormat.format(birthday.getTime()) + System.lineSeparator());
             birthday.add(Calendar.YEAR, 1);
             i++;
