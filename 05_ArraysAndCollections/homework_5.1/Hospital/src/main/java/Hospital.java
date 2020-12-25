f55835c43ea6b6618d15a08c234f6f9a26249ada
Andrey Kurochkin <barbedak@gmail.com>
2020-12-25 15:12:18 +0300

    homework_5.1 Hospital

--- 05_ArraysAndCollections/homework_5.1/Hospital/src/main/java/Hospital.java
+++ 05_ArraysAndCollections/homework_5.1/Hospital/src/main/java/Hospital.java
@@ -3,8 +3,12 @@ public class Hospital {
     public static float[] generatePatientsTemperatures(int patientsCount) {
 
         //TODO: напишите метод генерации массива температур пациентов
+        float[] patientTemp = new float[patientsCount];
+        for (int i = 0; i < patientsCount; i++){
+            patientTemp[i] = (float) (Math.random() * 8 + 32.0F);
+        }
 
-        return new float[0];
+        return patientTemp;
     }
 
     public static String getReport(float[] temperatureData) {
@@ -12,12 +16,17 @@ public class Hospital {
         TODO: Напишите код, который выводит среднюю температуру по больнице,количество здоровых пациентов,
             а также температуры всех пациентов.
         */
+        String report = "Температуры пациентов:";
+        float sumTemp = 0.0F;
+        int healthyPatient = 0;
+        for (float temp : temperatureData){
+            if (temp >= 36.2F && temp <= 36.9F) { healthyPatient++; }
+            report = report.concat(" " + temp);
+            sumTemp += temp;
+        }
 
-        String report =
-                "Температуры пациентов: " + 0 +
-                        "\nСредняя температура: " + 0 +
-                        "\nКоличество здоровых: " + 0;
-
+        float midTemp = (float) (Math.round(sumTemp / temperatureData.length * 10.0) / 10.0);
+        report = report.concat("\nСредняя температура: " + midTemp + "\nКоличество здоровых: " + healthyPatient);
         return report;
     }
 }
