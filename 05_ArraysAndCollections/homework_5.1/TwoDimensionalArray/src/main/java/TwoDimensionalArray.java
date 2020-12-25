f9fcdb13f4e1271d61c3d65e9ea82e584041fef8
Andrey Kurochkin <barbedak@gmail.com>
2020-12-25 16:37:47 +0300

    homework_5.1 TwoDimensionalArray

--- 05_ArraysAndCollections/homework_5.1/TwoDimensionalArray/src/main/java/TwoDimensionalArray.java
+++ 05_ArraysAndCollections/homework_5.1/TwoDimensionalArray/src/main/java/TwoDimensionalArray.java
@@ -9,6 +9,17 @@ public class TwoDimensionalArray {
         // [ , X,  ]
         // [X,  , X]
 
-        return new char[0][0];
+        char[][] array = new char[size][size];
+        for (int i=0; i < size; i++){
+            for (int j = 0; j < size; j++){
+                if (i==j || j == size - i - 1){
+                    array[i][j] = 'X';
+                } else {
+                    array[i][j] = ' ';
+                }
+            }
+        }
+
+        return array;
     }
 }
