e8f255625f46fff28467517348e103da290ecc94
Andrey Kurochkin <barbedak@gmail.com>
2020-12-25 12:56:47 +0300

    homework_5.1 ReverseArray

--- 05_ArraysAndCollections/homework_5.1/ReverseArray/src/main/java/ReverseArray.java
+++ 05_ArraysAndCollections/homework_5.1/ReverseArray/src/main/java/ReverseArray.java
@@ -2,6 +2,12 @@ public class ReverseArray {
 
     //TODO: Напишите код, который меняет порядок расположения элементов внутри массива на обратный.
     public static String[] reverse (String[] strings){
+        int lastElement = strings.length - 1;
+        for (int i = 0; i <= strings.length / 2; i++) {
+            String buffer = strings[i];
+            strings[i] = strings[lastElement - i];
+            strings[lastElement - i] = buffer;
+        }
         return strings;
     }
 }
