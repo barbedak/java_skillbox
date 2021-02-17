bb473db0121fc8de655e75c9246c5c1fd18ef663
Andrey Kurochkin <barbedak@gmail.com>
2021-02-17 23:56:28 +0300

    homework_5.4 PhoneBook

--- 05_ArraysAndCollections/homework_5.4/PhoneBook/src/main/java/Main.java
+++ 05_ArraysAndCollections/homework_5.4/PhoneBook/src/main/java/Main.java
@@ -1,5 +1,79 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        PhoneBook contacts = new PhoneBook();
+        String name;
+        String phone;
 
+        for (; ; ) {
+            System.out.println("Введите номер, имя или команду:");
+            String input = scanner.nextLine();
+
+            if (input.equals("LIST")) {
+                printPhoneBook(contacts);
+                continue;
+            }
+
+            //введен номер телефона
+            if (contacts.isValidPhone(input) && !contacts.isValidName(input)) {
+                phone = input;
+                boolean isNewPhone = !contacts.phoneBook.containsKey(phone);
+                if (isNewPhone) {
+                    System.out.println("Такого номера нет в телефонной книге.");
+                    System.out.println("Введите имя абонента для номера " + phone + ":");
+                    name = scanner.nextLine();
+                    if (contacts.isValidName(name)) {
+                        contacts.addContact(phone, name);
+                        System.out.println("Контакт сохранен!");
+                    }
+                } else {
+                    System.out.println(contacts.getNameByPhone(phone) + " - " + phone);
+                }
+            //введено имя
+            } else if (contacts.isValidName(input) && !contacts.isValidPhone(input)) {
+                name = input;
+                boolean isNewName = !contacts.phoneBook.containsValue(name);
+                if (isNewName) {
+                    System.out.println("Такого имени в телефонной книге нет.");
+                    System.out.println("Введите номер телефона для абонента " + name + ":");
+                    phone = scanner.nextLine();
+                    if (contacts.isValidPhone(phone)) {
+                        contacts.addContact(phone, name);
+                        System.out.println("Контакт сохранен!");
+                    }
+                } else {
+                    System.out.println(contacts.getPhonesByName(name));
+                }
+            } else {
+                System.out.println("Неверный формат ввода");
+            }
+        }
+    }
+
+    private static void printPhoneBook(PhoneBook contacts) {
+        String checkedName;
+        String name;
+        String contactNumbers;
+        ArrayList<String> checkedNameList = new ArrayList<>();
+        for (String phone : contacts.phoneBook.keySet()) {
+            contactNumbers = phone;
+            checkedName = contacts.phoneBook.get(phone);
+            for (String key : contacts.phoneBook.keySet()) {
+                name = contacts.phoneBook.get(key);
+                if (checkedNameList.contains(name)) {
+                    continue;
+                }
+                if (checkedName.equals(name) && !phone.equals(key)) {
+                    contactNumbers = contactNumbers.concat(", " + key);
+                }
+            }
+            if (!checkedNameList.contains(checkedName)) {
+                System.out.println(checkedName + " - " + contactNumbers);
+            }
+            checkedNameList.add(checkedName);
+        }
     }
 }
--- 05_ArraysAndCollections/homework_5.4/PhoneBook/src/main/java/PhoneBook.java
+++ 05_ArraysAndCollections/homework_5.4/PhoneBook/src/main/java/PhoneBook.java
@@ -1,29 +1,69 @@
 import java.util.Set;
+import java.util.TreeMap;
 import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PhoneBook {
+    public final TreeMap<String, String> phoneBook = new TreeMap<>(); //<phone : name>
+
+    public boolean isValidPhone(String phone){
+        Pattern patternNumbers = Pattern.compile("7[0-9]");
+        Matcher matcherNumbers = patternNumbers.matcher(phone);
+        if (phone.length() == 11 && matcherNumbers.find()){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isValidName(String name){
+        Pattern patternName = Pattern.compile("[A-ZА-Я][a-zа-я]+");
+        Matcher matcherName = patternName.matcher(name);
+        if (name.length() > 0 && matcherName.find()){
+            return true;
+        }
+        return false;
+    }
 
     public void addContact(String phone, String name) {
         // проверьте корректность формата имени и телефона
         // если такой номер уже есть в списке, то перезаписать имя абонента
+        if (isValidName(name) && isValidPhone(phone)){
+            phoneBook.put(phone, name);
+        }
     }
 
     public String getNameByPhone(String phone) {
         // формат одного контакта "Имя - Телефон"
         // если контакт не найдены - вернуть пустую строку
+        if (phoneBook.containsKey(phone)){
+            String contact;
+            contact = phoneBook.get(phone) + " - " + phone;
+            return contact;
+        }
         return "";
     }
 
     public Set<String> getPhonesByName(String name) {
         // формат одного контакта "Имя - Телефон"
         // если контакт не найден - вернуть пустой TreeSet
-        return new TreeSet<>();
+        TreeSet<String> contacts = new TreeSet<>();
+        for (String key : phoneBook.keySet()){
+            if (phoneBook.get(key).equals(name)){
+                contacts.add(name + " - " + key);
+            }
+        }
+        return contacts;
     }
 
     public Set<String> getAllContacts() {
         // формат одного контакта "Имя - Телефон"
         // если контактов нет в телефонной книге - вернуть пустой TreeSet
-        return new TreeSet<>();
+        TreeSet<String> contacts = new TreeSet<>();
+        for (String phone : phoneBook.keySet()){
+            contacts.add(phoneBook.get(phone) + " - " + phone);
+        }
+        return contacts;
     }
 
 }
