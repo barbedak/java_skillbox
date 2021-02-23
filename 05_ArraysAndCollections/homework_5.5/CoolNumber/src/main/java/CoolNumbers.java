6399572818b966065b77ff33489b69ebaa8354d8
Andrey Kurochkin <barbedak@gmail.com>
2021-02-24 00:09:26 +0300

    homework_5.5 CoolNumbers

--- 05_ArraysAndCollections/homework_5.5/CoolNumber/src/main/java/CoolNumbers.java
+++ 05_ArraysAndCollections/homework_5.5/CoolNumber/src/main/java/CoolNumbers.java
@@ -1,29 +1,72 @@
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.TreeSet;
+
+import java.util.*;
 
 public class CoolNumbers {
 
+    public static char getLetter() {
+        char[] letters = {'А','В','Е','К','М','Н','О','Р','С','Т','У','Х'};
+        int i = (int) (Math.random()*11);
+        return letters[i];
+    }
+
+    public static int getDigit() {
+        return (int) (Math.random()*10);
+    }
+
+    public static String getRegion() {
+        String region;
+        int digit = (int) (1 + Math.random()*199);
+        region = Integer.toString(digit);
+        if (region.length() < 2) {
+            region = "0" + region;
+        }
+        return region;
+    }
     public static List<String> generateCoolNumbers() {
-        return Collections.emptyList();
+        /*
+        * XYZ — различный набор из списка разрешенных букв, N — цифры, R — регион (от 01 до 199);
+          XNNNYZR — пример: A111BC197, У777HC66.
+          * буквы: А, В, Е, К, М, Н, О, Р, С, Т, У, Х
+          * */
+        String firstLetter;
+        String secondLetter;
+        String thirdLetter;
+        int digit;
+        String region;
+        String number;
+        List<String> numbersList = new ArrayList<>();
+        for (int i = 0; i < 2000003; i++ ) {
+            firstLetter = String.valueOf(getLetter());
+            secondLetter = String.valueOf(getLetter());
+            thirdLetter = String.valueOf(getLetter());
+            digit = getDigit();
+            region = getRegion();
+            number = firstLetter + digit + digit + digit + secondLetter + thirdLetter + region;
+            numbersList.add(number);
+        }
+        return numbersList;
     }
 
     public static boolean bruteForceSearchInList(List<String> list, String number) {
+        for (String elem : list){
+            if (elem.equals(number)) {
+                return true;
+            }
+        }
         return false;
     }
 
     public static boolean binarySearchInList(List<String> sortedList, String number) {
-        return false;
+        return Collections.binarySearch(sortedList, number) >= 0;
     }
 
 
     public static boolean searchInHashSet(HashSet<String> hashSet, String number) {
-        return false;
+        return hashSet.contains(number);
     }
 
     public static boolean searchInTreeSet(TreeSet<String> treeSet, String number) {
-        return false;
+        return treeSet.contains(number);
     }
 
 }
--- 05_ArraysAndCollections/homework_5.5/CoolNumber/src/main/java/Main.java
+++ 05_ArraysAndCollections/homework_5.5/CoolNumber/src/main/java/Main.java
@@ -1,12 +1,59 @@
+import java.util.*;
+
 public class Main {
-    /*
-    TODO:
-     - реализовать методы класса CoolNumbers
-     - посчитать время поиска введимого номера в консоль в каждой из структуры данных
-     - проанализоровать полученные данные
-     */
 
     public static void main(String[] args) {
+        long startTime;
+        long endTime;
+        boolean isNumberFind;
+        String searchNumber = "Р555ОК53";
+        List<String> carNumbers = CoolNumbers.generateCoolNumbers();
 
+        startTime = System.nanoTime();
+        isNumberFind = CoolNumbers.bruteForceSearchInList(carNumbers, searchNumber);
+        endTime = System.nanoTime();
+        System.out.print("Поиск перебором: ");
+        if (isNumberFind) {
+            System.out.print("номер найден");
+        } else {
+            System.out.print("номер не найден");
+        }
+        System.out.println(", поиск занял " + (endTime - startTime) + "нс");
+
+        Collections.sort(carNumbers);
+        startTime = System.nanoTime();
+        isNumberFind = CoolNumbers.binarySearchInList(carNumbers, searchNumber);
+        endTime = System.nanoTime();
+        System.out.print("Бинарный поиск: ");
+        if (isNumberFind) {
+            System.out.print("номер найден");
+        } else {
+            System.out.print("номер не найден");
+        }
+        System.out.println(", поиск занял " + (endTime - startTime) + "нс");
+
+        HashSet<String> carNumbersHash = new HashSet<>(carNumbers);
+        startTime = System.nanoTime();
+        isNumberFind = CoolNumbers.searchInHashSet(carNumbersHash, searchNumber);
+        endTime = System.nanoTime();
+        System.out.print("Поиск в HashSet: ");
+        if (isNumberFind) {
+            System.out.print("номер найден");
+        } else {
+            System.out.print("номер не найден");
+        }
+        System.out.println(", поиск занял " + (endTime - startTime) + "нс");
+
+        TreeSet<String> carNumbersTree = new TreeSet<>(carNumbers);
+        startTime = System.nanoTime();
+        isNumberFind = CoolNumbers.searchInTreeSet(carNumbersTree, searchNumber);
+        endTime = System.nanoTime();
+        System.out.print("Поиск в TreeSet: ");
+        if (isNumberFind) {
+            System.out.print("номер найден");
+        } else {
+            System.out.print("номер не найден");
+        }
+        System.out.println(", поиск занял " + (endTime - startTime) + "нс");
     }
 }
