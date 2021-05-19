0886e24bfffb70b15437fd5f8767905a5ff8b889
Andrey Kurochkin <barbedak@gmail.com>
2021-05-19 10:34:49 +0300

    homework_9.2 Employees

--- 09_AdvancedOOPFeatures/homework_9.2/Employees/src/main/java/Main.java
+++ 09_AdvancedOOPFeatures/homework_9.2/Employees/src/main/java/Main.java
@@ -1,3 +1,6 @@
+import java.util.Calendar;
+import java.util.Comparator;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 public class Main {
@@ -11,8 +14,11 @@ public class Main {
     }
 
     public static Employee findEmployeeWithHighestSalary(List<Employee> staff, int year) {
-        //TODO Метод должен вернуть сотрудника с максимальной зарплатой среди тех,
-        // кто пришёл в году, указанном в переменной year
-        return null;
+        return staff.stream()
+                .filter(e -> { Calendar calendar = new GregorianCalendar();
+                               calendar.setTime(e.getWorkStart());
+                               return calendar.get(Calendar.YEAR) == year;})
+                .max(Comparator.comparing(Employee::getSalary))
+                .get();
     }
 }
