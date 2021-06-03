eb455ba6729af94ba4abab7759ed2b44f953f04e
Andrey Kurochkin <barbedak@gmail.com>
2021-06-03 14:59:58 +0300

    homework_10.1 ConsoleCustomerList

--- 10_ExceptionsDebuggingAndTesting/homework_10.1/ConsoleCustomerList/src/main/java/CustomerStorage.java
+++ 10_ExceptionsDebuggingAndTesting/homework_10.1/ConsoleCustomerList/src/main/java/CustomerStorage.java
@@ -8,6 +8,17 @@ public class CustomerStorage {
         storage = new HashMap<>();
     }
 
+    private boolean checkEmail(String email){
+        return email.matches("[a-zA-Z_0-9.]+@[a-zA-Z_0-9]+\\.[a-zA-Z_0-9]+");
+    }
+
+    private boolean checkPhoneNumber (String number){
+        if (number.length() != 12) {
+            return false;
+        }
+        return number.matches("^\\+7[0-9]+");
+    }
+
     public void addCustomer(String data) {
         final int INDEX_NAME = 0;
         final int INDEX_SURNAME = 1;
@@ -15,6 +26,19 @@ public class CustomerStorage {
         final int INDEX_PHONE = 3;
 
         String[] components = data.split("\\s+");
+
+        if (components.length != 4) {
+            throw new IllegalArgumentException("Wrong amount of parameters!");
+        }
+
+        if (!checkEmail(components[INDEX_EMAIL])) {
+            throw new IllegalArgumentException("Wrong e-mail!");
+        }
+
+        if (!checkPhoneNumber(components[INDEX_PHONE])) {
+            throw new IllegalArgumentException("Wrong phone!");
+        }
+
         String name = components[INDEX_NAME] + " " + components[INDEX_SURNAME];
         storage.put(name, new Customer(name, components[INDEX_PHONE], components[INDEX_EMAIL]));
     }
@@ -24,7 +48,11 @@ public class CustomerStorage {
     }
 
     public void removeCustomer(String name) {
-        storage.remove(name);
+        try {
+            storage.remove(name);
+        } catch (ArrayIndexOutOfBoundsException ex){
+            System.out.println(ex.getMessage());
+        }
     }
 
     public Customer getCustomer(String name) {
