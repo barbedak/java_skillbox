5309d6e342409645558d93b05dbde8008400be3a
Andrey Kurochkin <barbedak@gmail.com>
2021-07-13 16:00:40 +0300

    homework_13.1 calculate folder size

--- 13_FilesAndNetwork/homework_13.1/src/main/java/FileUtils.java
+++ 13_FilesAndNetwork/homework_13.1/src/main/java/FileUtils.java
@@ -1,6 +1,23 @@
-public class FileUtils {
+import java.io.File;
 
-    public static long calculateFolderSize(String path) {
-        return 0;
+public class FileUtils {
+    public static long calculateFolderSize(String folderPath) {
+        long folderSize = 0;
+        File folder = new File(folderPath);
+        try {
+            String[] names = folder.list();
+            for (String path : names) {
+                path = folderPath + "\\" + path;
+                File file = new File(path);
+                if (file.isDirectory()) {
+                    folderSize += calculateFolderSize(path);
+                    continue;
+                }
+                folderSize += file.length();
+            }
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+        return folderSize;
     }
 }
