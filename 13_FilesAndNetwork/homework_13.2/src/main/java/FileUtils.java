584c9256d6fa055b139304fda4b8515682a7f3e3
Andrey Kurochkin <barbedak@gmail.com>
2021-07-21 19:18:11 +0300

    homework_13.2 copy files and subdirectory to folder

--- 13_FilesAndNetwork/homework_13.2/src/main/java/FileUtils.java
+++ 13_FilesAndNetwork/homework_13.2/src/main/java/FileUtils.java
@@ -1,5 +1,22 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class FileUtils {
-    public static void copyFolder(String sourceDirectory, String destinationDirectory) {
-        // TODO: write code copy content of sourceDirectory to destinationDirectory
+    public static void copyFolder(String sourceDirectory, String destinationDirectory) throws IOException {
+        File sourceFolder = new File(sourceDirectory);
+        String[] namesOfFilesInFolder = sourceFolder.list();
+        for (String fileName : namesOfFilesInFolder) {
+            File sourceFile = new File(sourceDirectory + "\\" + fileName);
+            if (sourceFile.isDirectory()) {
+                Files.createDirectory(Path.of(destinationDirectory + "\\" + fileName));
+                copyFolder(sourceFile.toPath().toString(), destinationDirectory + "\\" + fileName);
+                continue;
+            }
+            File destinationFile = new File(destinationDirectory + "\\" + fileName);
+            Files.copy(sourceFile.toPath(), destinationFile.toPath());
+        }
+        System.out.println("Файлы из '" + sourceDirectory + "' в '" + destinationDirectory + "' скопированы!");
     }
 }
--- 13_FilesAndNetwork/homework_13.2/src/main/java/Main.java
+++ 13_FilesAndNetwork/homework_13.2/src/main/java/Main.java
@@ -1,4 +1,26 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("Введите папку, откуда копировать : ");
+            String source = scanner.nextLine();
+            System.out.print("Введите папку, куда копировать : ");
+            String destination = scanner.nextLine();
+            File sourceFolder = new File(source);
+            File destinationFolder = new File(destination);
+            if (sourceFolder.isDirectory() && destinationFolder.isDirectory()) {
+                try {
+                    FileUtils.copyFolder(source, destination);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            } else {
+                System.out.println("Неправильно указана папка-источник или целевая папка!");
+            }
+        }
+
     }
 }
