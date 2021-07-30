3fa94c153db50830e230154a58958da448e04590
Andrey Kurochkin <barbedak@gmail.com>
2021-07-30 12:32:32 +0300

    homework_13.4 download img-files from website using InputStream and Files

--- 13_FilesAndNetwork/homework_13.4/src/main/java/Main.java
+++ 13_FilesAndNetwork/homework_13.4/src/main/java/Main.java
@@ -3,11 +3,12 @@ import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
-import java.nio.channels.Channels;
-import java.nio.channels.ReadableByteChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
 
 public class Main {
@@ -20,29 +21,17 @@ public class Main {
     }
 
     public static void downloadFile(String url) {
-        String nameFile = "images/" + url.replaceAll("https://icdn.lenta.ru/images", "");
-        createFoldersAndFile(nameFile);
-        try {
-            URL link = new URL(url);
-            ReadableByteChannel rbc = Channels.newChannel(link.openStream());
-            FileOutputStream fos = new FileOutputStream(nameFile);
-            fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
-            fos.close();
-            rbc.close();
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-    }
-
-    public static void createFoldersAndFile(String pathToFile) {
-        String pathFolder = pathToFile.replaceAll("/\\w+\\.jpg", "");
-        try {
-            File folder = new File(pathFolder);
-            folder.mkdirs();
-            File file = new File(pathToFile);
-            file.createNewFile();
-        } catch (Exception ex) {
-            ex.printStackTrace();
+        String fileName = "images/" + url.replaceAll("https://icdn.lenta.ru/images", "");
+        String folderPath = fileName.replaceAll("/\\w+\\.jpg", "");
+        File folder = new File(folderPath);
+        if (folder.mkdirs()) {
+            try {
+                URL website = new URL(url);
+                InputStream in = website.openStream();
+                Files.copy(in, Path.of(fileName), StandardCopyOption.REPLACE_EXISTING);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
