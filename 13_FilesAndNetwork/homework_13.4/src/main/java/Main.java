65ce3099cb977983a0a8dfa82c577decaeb84011
Andrey Kurochkin <barbedak@gmail.com>
2021-07-31 10:55:13 +0300

    homework_13.4 download img-files from website using Jsoup, InputStream and Files

--- 13_FilesAndNetwork/homework_13.4/src/main/java/Main.java
+++ 13_FilesAndNetwork/homework_13.4/src/main/java/Main.java
@@ -1,7 +1,6 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
-
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -14,7 +13,7 @@ import java.nio.file.StandardCopyOption;
 public class Main {
     public static void main(String[] args) throws IOException {
         Document doc = Jsoup.connect("https://lenta.ru/").get();
-        Elements element = doc.select("img.g-picture");
+        Elements element = doc.select("img[src$=.jpg]");
         System.out.println("Start downloading");
         element.forEach(el -> downloadFile(el.attr("abs:src")));
         System.out.println("Finish downloading");
