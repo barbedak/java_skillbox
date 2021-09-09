ae3d0d96e93e1e4f1f39596cf3e4f72c8b2258ec
Andrey Kurochkin <barbedak@gmail.com>
2021-09-09 10:22:48 +0300

    homework 14.1 find avg sales, using mysql. Fix wrong calculation avg in sql query

--- 14_SQLAndHibernate/src/main/java/Main.java
+++ 14_SQLAndHibernate/src/main/java/Main.java
@@ -8,13 +8,16 @@ public class Main {
         String url = "jdbc:mysql://localhost:3306/skillbox";
         String user = "root";
         String pass = "$Master";
+        String sqlQuery = "SELECT course_name, if( MAX(MONTH(subscription_date)) - MIN(MONTH(subscription_date)) = 1," +
+                " count(subscription_date) / 2, if( MAX(MONTH(subscription_date)) = MIN(MONTH(subscription_date)), " +
+                "count(subscription_date)," +
+                " count(subscription_date) / MAX(MONTH(subscription_date)) - MIN(MONTH(subscription_date)) )) as avg " +
+                "FROM Purchaselist GROUP BY course_name";
 
         try {
             Connection connection = DriverManager.getConnection(url, user, pass);
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT course_name, " +
-                "(count(subscription_date) / (MAX(MONTH(subscription_date)) - MIN(MONTH(subscription_date)))) as avg" +
-                " FROM Purchaselist GROUP BY course_name");
+            ResultSet resultSet = statement.executeQuery(sqlQuery);
 
             while (resultSet.next()) {
                 String courseName = resultSet.getString("course_name");
