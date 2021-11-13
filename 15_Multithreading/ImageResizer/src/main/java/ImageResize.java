41ccf1ddaad02dc735775724218c1dc969db2d09
Andrey Kurochkin <barbedak@gmail.com>
2021-11-13 18:11:04 +0300

    Homework 15.1 Image Resizer with imgscalr-lib

--- 15_Multithreading/ImageResizer/src/main/java/ImageResize.java
+++ 15_Multithreading/ImageResizer/src/main/java/ImageResize.java
@@ -1,3 +1,5 @@
+import org.imgscalr.Scalr;
+
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -5,10 +7,12 @@ import java.io.File;
 public class ImageResize implements Runnable {
     private File[] files;
     private String dstFolder;
+    private long start;
 
-    public ImageResize(File[] files, String dstFolder) {
+    public ImageResize(File[] files, String dstFolder, long start) {
         this.files = files;
         this.dstFolder = dstFolder;
+        this.start = start;
     }
 
     @Override
@@ -24,23 +28,13 @@ public class ImageResize implements Runnable {
                 int newHeight = (int) Math.round(
                         image.getHeight() / (image.getWidth() / (double) newWidth)
                 );
-                BufferedImage newImage = new BufferedImage(
-                        newWidth, newHeight, BufferedImage.TYPE_INT_RGB
-                );
 
-                int widthStep = image.getWidth() / newWidth;
-                int heightStep = image.getHeight() / newHeight;
-
-                for (int x = 0; x < newWidth; x++) {
-                    for (int y = 0; y < newHeight; y++) {
-                        int rgb = image.getRGB(x * widthStep, y * heightStep);
-                        newImage.setRGB(x, y, rgb);
-                    }
-                }
+                BufferedImage newImage = Scalr.resize(image, newWidth, newHeight);
 
                 File newFile = new File(dstFolder + "/" + file.getName());
                 ImageIO.write(newImage, "jpg", newFile);
             }
+            System.out.println("Duration: " + (System.currentTimeMillis() - start));
         } catch (Exception ex) {
             ex.printStackTrace();
         }
--- 15_Multithreading/ImageResizer/src/main/java/Main.java
+++ 15_Multithreading/ImageResizer/src/main/java/Main.java
@@ -1,31 +1,30 @@
 import java.io.File;
 
 public class Main {
+    private final static int NUMBER_OF_CORE = 4;
 
     public static void main(String[] args) {
         String srcFolder = "e:/from";
         String dstFolder = "e:/to";
-        int numberOfCore = 4;
 
         File srcDir = new File(srcFolder);
 
         long start = System.currentTimeMillis();
 
         File[] files = srcDir.listFiles();
-        int part = files.length / numberOfCore;
-        int startCopyPos = 0;
+        int part = files.length / NUMBER_OF_CORE;
+        int startCopyPosition = 0;
 
-        for (int i = 1; i < numberOfCore; i++) {
-            File[] files1 = new File[part];
-            System.arraycopy(files, startCopyPos, files1, 0, files1.length);
-            startCopyPos += files1.length;
-            new Thread(new ImageResize(files1, dstFolder)).start();
+        if (NUMBER_OF_CORE > 1) {
+            for (int i = 1; i < NUMBER_OF_CORE; i++) {
+                File[] files1 = new File[part];
+                System.arraycopy(files, startCopyPosition, files1, 0, files1.length);
+                startCopyPosition += files1.length;
+                new Thread(new ImageResize(files1, dstFolder, start)).start();
+            }
         }
-
-        File[] files1 = new File[files.length - part * numberOfCore + 1];
-        System.arraycopy(files, startCopyPos, files1, 0, files1.length);
-        new Thread(new ImageResize(files1, dstFolder)).start();
-
-        System.out.println("Duration: " + (System.currentTimeMillis() - start));
+        File[] files1 = new File[files.length - part * NUMBER_OF_CORE + part];
+        System.arraycopy(files, startCopyPosition, files1, 0, files1.length);
+        new Thread(new ImageResize(files1, dstFolder, start)).start();
     }
 }
