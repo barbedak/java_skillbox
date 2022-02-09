f092890f4a9aa70d1f0f470b7b89fc1f9225d145
Andrey Kurochkin <barbedak@gmail.com>
2022-02-09 19:42:39 +0300

    homework 17.21 ForkJoinPool. make site map. not working format output to file

--- Multithreading/ForkJoinPool/src/main/java/Main.java
+++ Multithreading/ForkJoinPool/src/main/java/Main.java
@@ -1,36 +1,41 @@
 import java.io.FileWriter;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ForkJoinPool;
 
 public class Main {
     public static final String ROOT_URL = "https://skillbox.ru/";
-    public static List<String> allLinks = new ArrayList<>();
-    public static Map<String, List<Site>> mapRootSite;
+    public static Map<String, List<String>> siteMap;
+    public static Set<String> nodeList = new HashSet<>();
 
     public static void main(String[] args) {
-        Site root = new Site(ROOT_URL);
         System.out.print("Make site map... ");
-        mapRootSite = new ForkJoinPool().invoke(new SiteNameParser(root));
+        nodeList.add(ROOT_URL);
+        siteMap = new ForkJoinPool().invoke(new SiteLinkParser(ROOT_URL));
         System.out.println("Done!");
+        save(ROOT_URL, "");
+    }
 
+    private static void save(String node, String prefix) {
+        writeToFile(node + "\n");
+//        prefix +="\t";
+        try {
+            List<String> children = siteMap.get(node);
+            for (String child : children) {
+                save(prefix + child, prefix+"\t");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void writeToFile(String link) {
         try {
             FileWriter writer = new FileWriter("data/out.txt", true);
-            writer.write(makeStringToSave(ROOT_URL));
+            writer.write(link);
             writer.flush();
             writer.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-    }
-
-    private static String makeStringToSave(String site) {
-        String out = site;
-        for (Site child : mapRootSite.get(site)) {
-            out.concat("\n\t" + makeStringToSave(child.getUrl()));
-        }
-        return out;
     }
 }
--- Multithreading/ForkJoinPool/src/main/java/Site.java
+++ Multithreading/ForkJoinPool/src/main/java/Site.java
@@ -2,33 +2,43 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Site {
     private String url;
-    private List<Site> children = new ArrayList<>();
+    private List<String> children = new ArrayList<>();
 
     public Site(String link) {
         this.url = link;
     }
 
-    public List<Site> getChildren() {
+    public List<String> getChildren() {
+        return children;
+    }
+
+    public void addChild(String child) {
+        children.add(child);
+    }
+
+    public void fillChildren() {
         try {
+            Thread.sleep(50);
             Document doc = Jsoup.connect(url).get();
-            Elements links = doc.select("a[href^=" + Main.ROOT_URL + "]");
+            Elements links = doc.select("a[href$=/]");
             links.forEach((link) -> {
-                String href = link.attr("abs:href");
-                if (!Main.allLinks.contains(href.trim())) {
-                    Main.allLinks.add(href);
-                    children.add(new Site(href));
+                String href = link.attr("abs:href").trim();
+                boolean isCorrectStartLink = href.startsWith(Main.ROOT_URL);
+                boolean isInRootLinks = Main.nodeList.contains(href);
+
+                if (isCorrectStartLink && !isInRootLinks) {
+                    children.add(href);
+                    Main.nodeList.add(href);
                 }
             });
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        return children;
     }
 
     public String getUrl() {
