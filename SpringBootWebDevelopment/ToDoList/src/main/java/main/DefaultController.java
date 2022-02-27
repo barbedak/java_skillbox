6b67b8c392e783331711a7b424a4f03e7cf6660d
Andrey Kurochkin <barbedak@gmail.com>
2022-02-27 12:43:49 +0300

    19_SpringBootWebDevelopment_ToDoList practice task #1 using args in @GetMapping

--- SpringBootWebDevelopment/ToDoList/src/main/java/main/DefaultController.java
+++ SpringBootWebDevelopment/ToDoList/src/main/java/main/DefaultController.java
@@ -1,15 +1,14 @@
 package main;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Date;
 
 @RestController
 public class DefaultController {
 
-    @RequestMapping("/")
-    public String index(){
-        return (new Date()).toString();
+    @GetMapping("/{text}&{count:\\d+}")
+    public String index(@PathVariable String text, @PathVariable int count){
+        return text.repeat(count);
     }
 }
